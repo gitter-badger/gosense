60db097664dac238bf247100f89b866b2c0a3a7a
ZhiFeng Hu <hufeng1987@gmail.com>
2015-11-09 09:52:41 +0800

    Fix sessions redeclare
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- admin-controller.go
+++ admin-controller.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang/groupcache/lru"
-	"github.com/gorilla/sessions"
 	"html/template"
 	"log"
 	"net/http"
