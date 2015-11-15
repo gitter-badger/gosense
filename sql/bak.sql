0561788fd5ee427afea038bd020d625ffc97bcb0
ZhiFeng Hu <hufeng1987@gmail.com>
2015-11-15 13:18:04 +0800

    Add table for record user visit

--- sql/bak.sql
+++ sql/bak.sql
@@ -4,8 +4,19 @@ CREATE TABLE `top_article` (
   `aid` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) CHARACTER SET utf8 DEFAULT '',
   `content` longtext CHARACTER SET utf8,
-  `publish_time` datetime DEFAULT current_timestamp,
-  `publish_status` tinyint(1) DEFAULT 1,
+  `publish_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `publish_status` tinyint(1) DEFAULT '1',
+  `views` int(11) DEFAULT '1',
   PRIMARY KEY (`aid`),
   FULLTEXT KEY `content` (`title`,`content`)
-) ENGINE=InnoDB AUTO_INCREMENT=3856 DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `top_vistors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `aid` int(11) DEFAULT '0',
+  `user_agent` varchar(512) DEFAULT NULL,
+  `ip` varchar(218) DEFAULT NULL,
+  `access_time` varchar(45) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `aid` (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
