211bcc314e81055e2531b7f2dbb306c99a5459f0
ZhiFeng Hu <hufeng1987@gmail.com>
2016-01-08 17:34:13 +0800

    Update grace restart
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- graceful-restart.sh
+++ graceful-restart.sh
@@ -4,7 +4,7 @@ if [ ! -f config.toml ]; then
     cp config.toml.dist config.toml
 fi
 if [ ! -z $1 ]; then
-    docker run --rm --name go-build -v $HOME/go:/go -v $(pwd):/www golang sh -c "cd /www ;go get -v ; go build -o /www/gosense "
+    docker run ${DNSSERVERS} --rm --name go-build -v $HOME/go:/go -v $(pwd):/www golang sh -c "cd /www ;go get github.com/jteeuwen/go-bindata/...;go get github.com/elazarl/go-bindata-assetfs/...;go-bindata-assetfs assets/... templates/...;go get -v ; go build -o /www/gosense "
 fi
 PIDGS=$(docker exec gosense pidof gosense)
 echo "Pid of gosense $PIDGS"
