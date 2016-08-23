3fd2f32802395b5b502cc661d6b5a6ad0c92c84b
ZhiFeng Hu <hufeng1987@gmail.com>
2016-08-23 11:28:27 +0800

    Update build.sh clean unused build args
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /www ;
-go get -u -v github.com/jteeuwen/go-bindata/...;
-go get -u -v github.com/elazarl/go-bindata-assetfs/...;
+go get -v github.com/jteeuwen/go-bindata/...;
+go get -v github.com/elazarl/go-bindata-assetfs/...;
 go-bindata-assetfs assets/... templates/...;
 go get -d -v ./...
 go build -ldflags "-linkmode external -extldflags -static" -o /www/gosense
